e4332f20dc91d3f3f6686b1c053bcf6028c50aa3
William Kwok <wkwok16@uw.edu>
2019-01-12 16:11:26 -0800

    upd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -152,23 +152,33 @@ class App extends Component {
 
     render() {
 
+        let dataForChart = this.state.data;
+
+        let dataForChartArr = Object.keys(dataForChart).map(d => {
+            return {
+                x: d,
+                y: dataForChart[d]
+            }
+        });
+
         let data = {
             labels: ['Scatter'],
             datasets: [
                 {
                     label: 'My First dataset',
-                    fill: false,
+                    fill: true,
                     backgroundColor: 'rgba(75,192,192,0.4)',
                     pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
+                    pointBackgroundColor: 'rgba(75,192,192,1)',
                     pointBorderWidth: 1,
                     pointHoverRadius: 5,
                     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
                     pointHoverBorderColor: 'rgba(220,220,220,1)',
                     pointHoverBorderWidth: 2,
-                    pointRadius: 1,
+
+                    pointRadius: 5,
                     pointHitRadius: 10,
-                    data: [{ x: 5, y: 10 }]
+                    data: dataForChartArr.length == 0 ? [] : dataForChartArr
                 }
             ],
             options: {
@@ -193,7 +203,7 @@ class App extends Component {
                 <Scatter className="m-3" data={data} />
 
                 <div style={{
-                    backgroundColor: "green",
+                    backgroundColor: "rgba(75,192,192,1)",
                     width: 600,
                     margin: 'auto',
                     // margin: 10,
